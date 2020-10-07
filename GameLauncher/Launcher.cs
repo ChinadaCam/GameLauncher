3e5fe7dc9cdf0c146a9e79475dff721de48258be
Faustino <sogamespt@gmail.com>
2020-10-07 01:21:44 +0100

    Initial commit

---
+++ GameLauncher/Launcher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+using System.Threading;
+
+namespace GameLauncher
+{
+    class Launcher
+    {
+        public static void PlayGame()
+        {
+
+            Process.Start("game.exe");
+         
+        }
+
+        public static void LaunchWebSite(string url)
+        {
+
+            ///  Process.Start(url);
+            System.Diagnostics.Process.Start("http://www.facebook.com");
+            
+        }
+
+
+    }
+}
---
+++ GameLauncher/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace GameLauncher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LaunchSiteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Launcher.LaunchWebSite("https://2vorvjknbrvtpgksqyrhyg-on.drv.tw/LimeSqueeze/GameLauncher/WebUpdates.html");
+        }
+
+        private void LaunchGameBtn_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+    }
+}
